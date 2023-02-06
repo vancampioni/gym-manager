5a0970fe1113ad5c6fe50e795a6beb4b0a597d39
vancampioni <vanessacrtsilva@gmail.com>
2023-02-06 07:18:20 -0300

    feat: building the exercise component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+
+import Exercises from './components/Exercises';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [exercise, setExercise] = useState([
+    {
+      id: '1',
+      title: 'Push-Ups',
+      repetitions: 10,
+      weigth: 'body'
+    },
+    {
+      id: '2',
+      title: 'Sit-ups',
+      repetitions: 10,
+      weigth: '15kg'
+    },
+  ]);
+
   return (
     <>
-      Hello World!
+      <div className='container'>
+        <Exercises />
+        
+      </div>
     </>
   );
 }
---
+++ src/components/Exercises.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Exercises = () => {
+    return <h1>Exercício 1</h1>
+};
+
+export default Exercises;
